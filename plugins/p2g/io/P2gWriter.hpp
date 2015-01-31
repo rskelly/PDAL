86db3448ed00d5d3425a983c04de95bbe4db3f4e
Rob <rob@dijital.ca>
2015-01-30 19:56:04 -0800

    Added interp mode to P2gWriter, to enable in memory processing. Requires p2g with 5-argument signature for CoreInterp.

--- plugins/p2g/io/P2gWriter.hpp
+++ plugins/p2g/io/P2gWriter.hpp
@@ -1,36 +1,36 @@
 /******************************************************************************
-* Copyright (c) 2011, Howard Butler, hobu.inc@gmail.com
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following
-* conditions are met:
-*
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in
-*       the documentation and/or other materials provided
-*       with the distribution.
-*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
-*       names of its contributors may be used to endorse or promote
-*       products derived from this software without specific prior
-*       written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-* OF SUCH DAMAGE.
-****************************************************************************/
+ * Copyright (c) 2011, Howard Butler, hobu.inc@gmail.com
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
 
 #pragma once
 
@@ -44,63 +44,57 @@
 #include <points2grid/config.h>
 #include <points2grid/Interpolation.hpp>
 #include <points2grid/Global.hpp>
-#include <points2grid/OutCoreInterp.hpp>
+#include <points2grid/CoreInterp.hpp>
 
-namespace pdal
-{
+namespace pdal {
 
-
-class p2g_error : public pdal_error
-{
+class p2g_error: public pdal_error {
 public:
-    p2g_error(std::string const& msg)
-        : pdal_error(msg)
-    {}
+	p2g_error(std::string const& msg) :
+			pdal_error(msg) {
+	}
 };
 
-
-
-class CoreInterp;
-
-class PDAL_DLL P2gWriter : public pdal::Writer
-{
+class PDAL_DLL P2gWriter: public pdal::Writer {
 public:
-    SET_STAGE_NAME("writers.p2g", "Points2Grid Writer")
-    SET_STAGE_LINK("http://pdal.io/stages/writers.p2g.html")
-    SET_PLUGIN_VERSION("1.0.0b1")
+	SET_STAGE_NAME("writers.p2g", "Points2Grid Writer")SET_STAGE_LINK("http://pdal.io/stages/writers.p2g.html")
+	SET_PLUGIN_VERSION("1.0.0b1")
 
-    P2gWriter() : Writer(), m_outputTypes(0), m_outputFormat(OUTPUT_FORMAT_ARC_ASCII) {};
-    ~P2gWriter() {};
+	P2gWriter() : Writer(), m_outputTypes(0), m_outputFormat(OUTPUT_FORMAT_ARC_ASCII) {};
+	~P2gWriter() {};
 
-    static Options getDefaultOptions();
+	static Options getDefaultOptions();
 
 private:
-    P2gWriter& operator=(const P2gWriter&); // not implemented
+	P2gWriter& operator=(const P2gWriter&); // not implemented
 
-    virtual void processOptions(const Options& options);
-    virtual void ready(PointContextRef ctx) {};
-    virtual void write(const PointBuffer& buf);
-    virtual void done(PointContextRef ctx) {};
-    virtual void initialize() {};
+	virtual void processOptions(const Options& options);
+	virtual void ready(PointContextRef ctx) {};
+	virtual void write(const PointBuffer& buf);
+	virtual void done(PointContextRef ctx) {};
+	virtual void initialize() {};
 
-    std::unique_ptr<OutCoreInterp> m_interpolator;
-    uint64_t m_pointCount;
+	std::unique_ptr<CoreInterp> m_interpolator;
+	std::string m_interp_mode;
 
-    uint32_t m_GRID_SIZE_X;
-    uint32_t m_GRID_SIZE_Y;
+	uint64_t m_pointCount;
 
-    double m_GRID_DIST_X;
-    double m_GRID_DIST_Y;
+	uint32_t m_GRID_SIZE_X;
+	uint32_t m_GRID_SIZE_Y;
 
-    double m_RADIUS_SQ;
-    unsigned int m_outputTypes;
-    uint32_t m_fill_window_size;
-    BOX3D m_bounds;
+	double m_GRID_DIST_X;
+	double m_GRID_DIST_Y;
 
-    std::string m_filename;
-    int m_outputFormat;
+	double m_RADIUS_SQ;
+	unsigned int m_outputTypes;
+	uint32_t m_fill_window_size;
+	BOX3D m_bounds;
 
-    std::vector<boost::tuple<double, double, double> > m_coordinates;
+	std::string m_filename;
+	int m_outputFormat;
+
+	std::vector<boost::tuple<double, double, double> > m_coordinates;
 };
 
-} // namespaces
+}
+ // namespaces
